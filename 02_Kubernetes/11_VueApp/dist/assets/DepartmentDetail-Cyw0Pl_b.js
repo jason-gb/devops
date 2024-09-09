f4f746e726da64d90ef07964db2142c4d83e4791
jason-gb <a01071865081@gmail.com>
2024-09-09 10:03:37 +0900

    jenkins

---
+++ 02_Kubernetes/11_VueApp/dist/assets/DepartmentDetail-Cyw0Pl_b.js
@@ -0,0 +1,1 @@
+import{u as n,c as s,a as e,t as a,h as l,o as u}from"./index-C9GZoySm.js";const r=e("h2",null,"학과 상세 조회",-1),m={__name:"DepartmentDetail",setup(o){const t=n();return(p,c)=>(u(),s("div",null,[r,e("p",null,a(l(t).fullPath),1),e("p",null,a(l(t).path),1),e("p",null,a(l(t).params),1),e("p",null,a(l(t).params.no),1),e("p",null,a(l(t).query),1)]))}};export{m as default};
---
+++ 02_Kubernetes/11_VueApp/dist/assets/Home-xu4WMtGg.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c,a as t}from"./index-C9GZoySm.js";const s={},n=t("h2",null,"HOME",-1),_=[n];function a(r,l){return c(),o("div",null,_)}const f=e(s,[["render",a]]);export{f as default};
